ba49c7dc7758e91ee07cbad319b2537bfb96b48f
Gary Riger <grigerny@aol.com>
2015-03-16 00:10:57 -0400

    added email validation regex

--- app/models/funding.rb
+++ app/models/funding.rb
@@ -1,3 +1,6 @@
 class Funding < ActiveRecord::Base
 	validates_presence_of :link, :title, :body, :contact_info
+
+	validates_format_of :contact_info,:multiline => true, :with => /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i
+
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,6 @@
 class Job < ActiveRecord::Base
 	validates_presence_of  :title, :body, :contact_info, :location, :compensation
+
+	validates_format_of :contact_info,:multiline => true, :with => /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i
+
 end
