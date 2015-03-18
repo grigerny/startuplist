c089586a073b11f9e5d52ddaf86c8e7cf6eae4e1
Gary Riger <grigerny@aol.com>
2015-03-17 21:58:45 -0400

    update

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   
   devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks' }
   match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
-  get 'logout' => 'devise/sessions#destroy'
   
 
   resources :users
