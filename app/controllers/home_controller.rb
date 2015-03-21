7a79297dad75e65dc5854cf3e9d0a08f47af4a2f
Gary Riger <grigerny@aol.com>
2015-03-21 17:00:32 -0400

    updates

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,21 +1,6 @@
 class HomeController < ApplicationController
 	def index
-	@post = Post.last
-    
-    @accelerators = Accelerator.all
-    @events = Event.all
-    @founders = Founder.all
-    @fundings = Funding.all
-    @investors = Investor.all
-    @jobs = Job.all
-    @marketplaces = Marketplace.all
-    @pitches = Pitch.all
-    @spaces = Space.all
-    @stresses = Stress.all
-    @questions = Question.all
-
-    @activities = (@accelerators + @questions + @events + @founders + @fundings + @investors + @jobs + @marketplaces + @pitches + @spaces + @stresses).sort_by {|a| a.created_at}.reverse
-	
+	  @post = Post.last
     q = params[:q]
     @founders = Founder.search(title_cont: q).result
     @jobs = Job.search(title_cont: q).result
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def activities
+    @accelerators = Accelerator.all
+    @events = Event.all
+    @founders = Founder.all
+    @fundings = Funding.all
+    @investors = Investor.all
+    @jobs = Job.all
+    @marketplaces = Marketplace.all
+    @pitches = Pitch.all
+    @spaces = Space.all
+    @stresses = Stress.all
+    @questions = Question.all
+
+    @activities = (@accelerators + @questions + @events + @founders + @fundings + @investors + @jobs + @marketplaces + @pitches + @spaces + @stresses).sort_by {|a| a.created_at}.reverse
+  end
 end
